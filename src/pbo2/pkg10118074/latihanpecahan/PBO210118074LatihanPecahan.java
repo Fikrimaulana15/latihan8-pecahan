b34b3a409baebd3b18181712724a024726ccb44c
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-09-26 16:57:02 +0700

    selesai

---
+++ src/pbo2/pkg10118074/latihanpecahan/PBO210118074LatihanPecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihanpecahan;
+
+/**
+ *
+ * @author 
+ * NAMA : FikriMaulana
+ * KELAS : IF-2
+ * NIM: 10118074
+ */
+public class PBO210118074LatihanPecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float : " + f);
+        System.out.println("double : " + d);
+        
+    }
+    
+}
